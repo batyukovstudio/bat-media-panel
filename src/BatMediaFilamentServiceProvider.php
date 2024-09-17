742705a0c226465dc65aa0e78793f5cfd4f8fa20
Dolzhenkov-Nikita <Dolzhenkov.na@gmail.com>
2024-09-17 12:18:48 +0500

    fix(ServiceProvider):/../Filament/Resources

--- src/BatMediaFilamentServiceProvider.php
+++ src/BatMediaFilamentServiceProvider.php
@@ -31,7 +31,7 @@ class BatMediaFilamentServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
 
             $this->publishes([
-                __DIR__.'/../Filament' => app_path('Filament/'),
+                __DIR__.'/../Filament/Resources' => ('./app/Filament/Resources'),
             ], 'bat-components');
         }
     }
